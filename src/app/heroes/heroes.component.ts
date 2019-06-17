b44f85ece1a4c3374c14c2972780692bdfb5ff41
Christa Stephens <christastephens@quickenloans.com>
2019-06-17 16:07:08 -0400

    displaying exported data

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,16 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import {Hero} from './hero';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss']
 })
+/*
+read as information to be exported
+information must match exported file when placing
+into export class
+*/
 export class HeroesComponent implements OnInit {
-
+hero: Hero = {
+  Id: 1,
+  Name: "Windstorm"
+};
   constructor() { }
 
   ngOnInit() {
   }
-
-  hero = "Windstorm";
 }
